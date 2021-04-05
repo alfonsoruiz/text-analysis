ff2c000aaf4436041cdbcd07f90306b38458607b
alfonsoruiz <1800ruiz@gmail.com>
2021-04-05 05:25:32 -0700

    change post path on client

--- src/client/js/getData.js
+++ src/client/js/getData.js
@@ -2,7 +2,7 @@ const getData = async (url) => {
   Client.renderLoader(document.getElementById('result'));
 
   try {
-    const {data, sourceUrl} = await Client.postData('http://localhost:8081/api', url);
+    const {data, sourceUrl} = await Client.postData('/api', url);
     Client.processResults(data, sourceUrl);
   } catch(e) {
     console.error(e);
